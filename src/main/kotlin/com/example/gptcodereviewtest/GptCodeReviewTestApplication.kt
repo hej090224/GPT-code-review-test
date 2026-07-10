349b1f3c5473b5cc767668b3c65cd9aeecd63c3d
hej090224 <fc49854985@gmail.com>
2026-07-10 09:20:53 +0900

    init

---
+++ src/main/kotlin/com/example/gptcodereviewtest/GptCodeReviewTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gptcodereviewtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GptCodeReviewTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<GptCodeReviewTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gptcodereviewtest/GptCodeReviewTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gptcodereviewtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GptCodeReviewTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
